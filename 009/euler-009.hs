7abc1dc2fedc8c047d1ddfb794cd3c3af6230e88
Rodrigo López Dato <rlopezdato@gmail.com>
2013-08-08 03:41:20 -0300

    Added Euclid's formula solution for Problem 9

--- 009/euler-009.hs
+++ 009/euler-009.hs
@@ -2,7 +2,13 @@
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 
-Brute-force time!
+Using Euclid's formula to generate all Pythagorean triplets.
 -}
 
-main = print $ head [(a,b,c) | a <- [1..1000], b <- [a..1000], c <- [b..1000], a^2 + b^2 == c^2, a + b + c == 1000]
+triples = [[a, b, c] | m <- [1..],
+					   n <- [1..m-1],
+					   let a = m^2 - n^2,
+					   let b = 2*m*n,
+					   let c = m^2 + n^2]
+
+main = print $ head [product t | t <- triples, sum t == 1000]
