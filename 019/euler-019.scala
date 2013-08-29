71ba52f4c2e38f6871730d358f8cd60627253c5a
Rodrigo López Dato <rld@utnso.(none)>
2013-08-28 23:39:32 -0300

    Problem 19 (Scala, imperative)

---
+++ 019/euler-019.scala
@@ -0,0 +1,20 @@
+/*
+How many Sundays fell on the first of the month during the twentieth
+century (1 Jan 1901 to 31 Dec 2000)?
+*/
+
+import org.joda.time._ // Pre-invented wheel
+
+var sundays = 0
+var start = new DateTime("1901-1-1")
+val end = new DateTime("2000-12-31")
+
+// Imperative solution
+while (start isBefore end) {
+	if (start.getDayOfWeek == DateTimeConstants.SUNDAY) {
+		sundays += 1
+	}
+	start = start.plusMonths(1)
+}
+
+println(sundays)
