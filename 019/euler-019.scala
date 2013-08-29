c2cbc9d0bd33f70b481ffffbaa2dadc136d0679a
Rodrigo López Dato <rlopezdato@frba.utn.edu.ar>
2013-08-28 23:41:20 -0300

    Added problem 19 description comment

--- 019/euler-019.scala
+++ 019/euler-019.scala
@@ -1,6 +1,8 @@
 /*
 How many Sundays fell on the first of the month during the twentieth
 century (1 Jan 1901 to 31 Dec 2000)?
+
+Imperative solution. Requires org.joda.time and org.joda.joda-convert
 */
 
 import org.joda.time._ // Pre-invented wheel
